daac945a16cfff478e33cf290b296e2b7579a053
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2025-10-29 01:17:38 +0300

    refactor: remove sitecheck.py from root (moved to src/)

--- sitecheck.py
+++
@@ -1,7 +0,0 @@
-import requests
-class Client():
-	def __init__(self):
-		self.api="https://sitecheck.sucuri.net/api/v3"
-		self.headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36","x-requested-with": "XMLHttpRequest"}
-	def scan_url(self,url):
-		return requests.get(f"{self.api}/?scan={url}",headers=self.headers).json()
